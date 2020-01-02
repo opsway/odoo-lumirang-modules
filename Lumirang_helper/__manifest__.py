0d695ece1e9c1f79a3d9fbbb25e61c04c95c0571
Artem Shelest <artem.e.shelest@gmail.com>
2020-01-02 15:15:58 +0200

    [INT-101][REF] bump version

--- Lumirang_helper/__manifest__.py
+++ Lumirang_helper/__manifest__.py
@@ -10,7 +10,7 @@
     'author': "Ed Chu",
     'website': "http://www.lumirang.com",
     'category': "Web",
-    'version': "1.0",
+    'version': "1.0.1",
 
     'depends': [],
     'sequence': 0,
