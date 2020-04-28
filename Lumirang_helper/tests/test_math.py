e7085a1a0f08c0c36faf9a0f309b65f9fdefc445
edison12347 <eduard.chudaikin@gmail.com>
2020-04-28 14:38:30 +0300

    [RAYIM-527][ADD] round half up tests

--- Lumirang_helper/tests/test_math.py
+++ Lumirang_helper/tests/test_math.py
@@ -5,21 +5,22 @@ from ..functions.math_helper import round_half_up
 
 class TestMath(unittest.TestCase):
 
-    def test_round_half_up_low(self):
+    def test_round_half_up(self):
         # pylint: disable=no-self-use
-        assert math.isclose(round_half_up(1.124, 2), 1.12, rel_tol=1e-04)
-
-    def test_round_half_up_mid(self):
-        # pylint: disable=no-self-use
-        assert math.isclose(round_half_up(1.125, 2), 1.13, rel_tol=1e-04)
-
-    def test_round_half_up_mid_high(self):
-        # pylint: disable=no-self-use
-        assert math.isclose(round_half_up(1.126, 2), 1.13, rel_tol=1e-04)
-
-    def test_round_half_up_zero(self):
-        # pylint: disable=no-self-use
-        assert math.isclose(round_half_up(0, 2), 0, rel_tol=1e-04)
+        for x, decimal, result in [
+            (1.124, 2, 1.12),
+            (1.125, 2, 1.13),
+            (1.126, 2, 1.13),
+            (0, 2, 0),
+            (2, 2, 2),
+            (12, 2, 12),
+            (2, 0, 2),
+            (2.5, 0, 3),
+            (12.5, -1, 10),
+        ]:
+            self.assertAlmostEqual(round_half_up(x, decimal), result, 3,
+                                   msg='Failed for input {}, output {} with decimal places {}'.format(x, result,
+                                                                                                      decimal))
 
     def test_round_half_up_negative(self):
         # pylint: disable=no-self-use
