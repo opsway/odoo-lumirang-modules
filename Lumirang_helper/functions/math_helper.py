fd9ddbf329423be64c5cb2addc9c03c6df0d20a5
edison12347 <eduard.chudaikin@gmail.com>
2020-04-28 14:23:51 +0300

    [RAYIM-527][ADD] round half up

--- Lumirang_helper/functions/math_helper.py
+++ Lumirang_helper/functions/math_helper.py
@@ -3,4 +3,6 @@ import math
 
 def round_half_up(n, decimals=0):
     multiplier = 10 ** decimals
+    if n < 0:
+        raise ValueError('Only positive values are acceptable')
     return math.floor(n*multiplier + 0.5) / multiplier
--- Lumirang_helper/tests/test_math.py
+++ Lumirang_helper/tests/test_math.py
@@ -3,7 +3,7 @@ import math
 from ..functions.math_helper import round_half_up
 
 
-class TestDate(unittest.TestCase):
+class TestMath(unittest.TestCase):
 
     def test_round_half_up_low(self):
         # pylint: disable=no-self-use
@@ -16,3 +16,11 @@ class TestDate(unittest.TestCase):
     def test_round_half_up_mid_high(self):
         # pylint: disable=no-self-use
         assert math.isclose(round_half_up(1.126, 2), 1.13, rel_tol=1e-04)
+
+    def test_round_half_up_zero(self):
+        # pylint: disable=no-self-use
+        assert math.isclose(round_half_up(0, 2), 0, rel_tol=1e-04)
+
+    def test_round_half_up_negative(self):
+        # pylint: disable=no-self-use
+        self.assertRaises(ValueError, lambda: math.isclose(round_half_up(-3, 2), 0, rel_tol=1e-04))
