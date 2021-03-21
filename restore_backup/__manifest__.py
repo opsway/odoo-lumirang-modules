04acd294cf7dcf5941804391121919dd0e65a2ff
Artem Shelest <arshe@opsway.com>
2021-03-21 17:20:20 +0000

    [SIGHT-947][ADD] backup and restore modules

---
+++ restore_backup/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': "Database restore",
+
+    'summary': 'Backups local restore',
+
+    'description': "Restore backups from local files",
+
+    'author': "Artem Shelest",
+    'website': "http://www.lumirang.com",
+    'category': 'Administration',
+    'version': '13.0.1.0',
+    'installable': True,
+
+    'depends': ['base'],
+
+    'data': [],
+}
