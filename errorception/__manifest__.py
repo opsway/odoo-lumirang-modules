269913846b01b2900ecd669dc5828545e9ae39dd
Artem Shelest <arshe@opsway.com>
2021-05-11 11:17:50 +0100

    [SIGHT-1317][ADD] errorception module

---
+++ errorception/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': "ErrorCeption",
+
+    'summary': "ErrorCeption Integration",
+
+    'description': "Add ErrorCeption code to the frontend.",
+
+    'author': "Artme Shelest",
+    'website': "http://www.lumirang.com",
+    'category': "Website",
+    'version': "13.0.1.0",
+
+    'depends': ['web'],
+    'data': [
+        'views/assets.xml',
+        'views/res_config_settings.xml',
+    ],
+    'sequence': 2,
+    'application': True,
+}
