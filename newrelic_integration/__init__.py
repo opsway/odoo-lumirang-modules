3d3c33b5fc9984358af49c9eb7658632b9eb2bbb
Artem Shelest <arshe@opsway.com>
2021-06-24 08:38:25 +0100

    [SIGHT][FIX] newrelic

--- newrelic_integration/__init__.py
+++ newrelic_integration/__init__.py
@@ -1,5 +1,4 @@
-# -*- encoding: UTF-8 -*-
-from newrelic.api.exceptions import ConfigurationError
+from traceback import format_exc
 
 from . import controllers, models
 import os
@@ -23,19 +22,26 @@ try:
     else:
         import odoo.tools.config as config
         import newrelic.agent
+        from newrelic.api.exceptions import ConfigurationError
 
         env_value = os.environ.get('ODOO_STAGE', "dev")
         _logger.info("NewRelic environment is {}".format(env_value))
-        try:
-            dirname = os.path.dirname(__file__)
-            newrelic.agent.initialize(os.path.join(dirname, 'newrelic.ini'), env_value)
-        except (KeyError, ConfigurationError, OSError):
+        dirname = os.path.dirname(__file__)
+        filename = os.path.join(dirname, 'newrelic.ini')
+        if not os.path.exists(filename):
+            filename = config.get('new_relic_config_file', None)
+            if filename and not os.path.exists(filename):
+                filename = None
+        did_init = False
+        if filename:
             try:
-                _logger.info('NewRelic setting up from odoo config')
-                newrelic.agent.initialize(config['new_relic_config_file'], env_value)
-            except (KeyError, ConfigurationError, OSError):
-                _logger.info('NewRelic setting up from env variables')
-                newrelic.agent.initialize(environment=env_value)
+                _logger.info("NewRelic init from file {}".format(filename))
+                newrelic.agent.initialize(filename, env_value)
+                did_init = True
+            except (ConfigurationError, OSError):
+                _logger.info("NewRelic failed to init from file {}, {}".format(filename, format_exc()))
+        if not did_init:
+            newrelic.agent.initialize(environment=env_value)
 
         # Main WSGI Application
         target._nr_instrumented = True
@@ -77,4 +83,4 @@ try:
         target = odoo.http.WebRequest
         target._handle_exception = _nr_wrapper_handle_exception_(target._handle_exception)
 except ImportError:
-    _logger.warn('newrelic python module not installed or other missing module')
+    _logger.warn("newrelic python module not installed or other missing module")
