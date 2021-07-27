02ba77152b1791e85e4f6296f3751848e4e3b7a4
Artem Shelest <arshe@opsway.com>
2021-07-27 17:05:47 +0100

    [SS][REM] newrelic module

--- newrelic_integration/static/src/js/crash_manager_service.js
+++
@@ -1,8 +0,0 @@
-odoo.define('newrelic.CrashManagerService', function (require) {
-    "use strict";
-
-    const {CrashManager} = require('newrelic.CrashManager');
-    const core = require('web.core');
-
-    core.serviceRegistry.add('crash_manager', CrashManager);
-});
